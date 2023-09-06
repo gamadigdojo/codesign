a94435a9e718f9e177c1749fdd28c6c12ea5264d
gamadigdojo <gamadigdojo@gmail.com>
2023-09-06 08:32:40 +0700

    codesign day 2

---
+++ script.js
@@ -0,0 +1,13 @@
+// const textElement = document.getElementById('typing-text');
+// const textToType = "Temui komunitas yang berkembang di Care yang siap mendukungmu.";
+// let charIndex = 0;
+
+// function typeText() {
+//     if (charIndex < textToType.length) {
+//         textElement.textContent += textToType.charAt(charIndex);
+//         charIndex++;
+//         setTimeout(typeText, 100); // Adjust typing speed (lower value for faster typing)
+//     }
+// }
+
+// typeText(); // Start the typing animation
